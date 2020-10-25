c0dd0855d1fff9a14ae08dc6f73daf71b55ad7ad
SamYul2020 <73409267+SamYul2020@users.noreply.github.com>
2020-10-24 19:43:57 -0500

    versión 20.1

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -18,8 +18,8 @@
 	<nav>
 
 		<ul>
-			<li class="{{setActive('about') }}"><a href="/about">About</a></li>
-			<li class="{{setActive('contact') }}"><a href="/contact">Contact</a></li>
+			<li class="{{setActive('about') }}"><a href="/about">Acerca de</a></li>
+			<li class="{{setActive('contact') }}"><a href="/contact">Contacto</a></li>
 			<li class="{{setActive('home') }}"><a href="/">Home</a></li>
 			<li class="{{setActive('portfolio') }}"><a href="/portfolio">Portfolio</a></li>
 
